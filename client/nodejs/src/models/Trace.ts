bc2f6d41acceacfe0310fec6621841c58b02c689
kuchuk-borom-debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2026-05-09 19:25:58 +0530

    feat: add constructor to Trace model for initialization

--- client/nodejs/src/models/Trace.ts
+++ client/nodejs/src/models/Trace.ts
@@ -2,4 +2,10 @@ export class Trace {
     readonly name: string;
     readonly id: string;
     readonly createdAt: string;
+
+    constructor(name: string, id: string, createdAt: string) {
+        this.name = name;
+        this.id = id;
+        this.createdAt = createdAt;
+    }
 }
